fff4b8db64727178b8208fcdfd3995c4e3388bca
JosephLiao542211 <billan452211@gmail.com>
2024-05-18 14:40:16 -0400

    Merge stuff

--- BetterHH/src/Marketplace.tsx
+++ BetterHH/src/Marketplace.tsx
@@ -7,6 +7,7 @@ import ScrollableList from "./ScrollableList"
 function Marketplace() {
   return (
     <div className="z-6 h-full w-full bg-[#141416] flex items-center justify-center p-4">
+        <div className="h1"> Global Goals </div>
       <div className="w-[70%] bg-[#1f1f1f] p-4 m-4 overflow-hidden">
         <ScrollableList />
       </div>
