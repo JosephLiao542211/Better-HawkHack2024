e6d7a6516bb0ac3676ec9eb00b7882a072eb83f0
JosephLiao542211 <billan452211@gmail.com>
2024-05-18 13:43:21 -0400

    Draft 1

---
+++ BetterHH/src/Button.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ScrollableList from "./ScrollableList"
+
+function handleclick(){
+    return
+}
+
+function Button() {
+  return (
+    <button
+  type="button"
+  data-twe-ripple-init
+  onClick={handleclick}
+  data-twe-ripple-color="light"
+  className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+  Click me
+</button>
+  )
+}
+
+export default Button
--- BetterHH/src/main.tsx
+++ BetterHH/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Test from './Test.tsx'
+import Marketplace from './Marketplace.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Marketplace></Marketplace>
+    {/* <Test></Test> */}
   </React.StrictMode>,
 )
